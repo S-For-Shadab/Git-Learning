6b6fed32ef1c2f8dda290ff3a48fa8d049a71d4f
S-FOR-Shadab <khanshadab8829@gmail.com>
2022-11-16 19:15:56 +0530

    initial commit

--- Sum.java
+++ Sum.java
@@ -1,7 +1,8 @@
 class Sum{
     public static void main(String [] args){
         int a=5,b=6;
+        
 
-        System.out.print(a+b);
+        System.out.print("the sum of thw two number is "+(a+b));
     }
 }
